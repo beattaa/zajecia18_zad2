6243214542c51f8fa88ded746f95a10df623d977
beata <beata_sadek@o2.pl>
2020-06-08 09:56:18 +0200

    zajecia 18 - zadanie 2: Filtrowanie i przetwarzanie

--- src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
@@ -1,23 +1,37 @@
 package pl.javastart.exercise.streams;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilteringMachine {
 
     public List<Integer> filterOutNotEvenNumbers(List<Integer> numberList) {
-        return null;
+
+        return numberList.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> filterOutLowerNumbersThan20(List<Integer> numberList) {
-        return null;
+
+        return numberList.stream()
+                .filter(n -> n >= 20)
+                .collect(Collectors.toList());
     }
 
     public List<Book> convertToBooks(List<String> titles) {
-        return null;
+
+        return titles.stream()
+                .map(title -> new Book(title))
+                .collect(Collectors.toList());
     }
 
     // Metoda powinna przekształcić tytuły na książki i zwrócić tylko te które rozpoczynają się od słowa "Gra"
     public List<Book> convertToBooksAndReturnOnlyStartingWithGra(List<String> titles) {
-        return null;
+
+        return titles.stream()
+                .map(title -> new Book(title))
+                .filter(book -> book.getTitle().startsWith("Gra"))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
@@ -1,15 +1,24 @@
 package pl.javastart.exercise.streams;
 
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilteringMachineTwo {
 
     // metoda powinna zwrócić imiona wszystkich niedorosłych osób (mniej niż 18 lat)
     public List<String> findKidNames(List<Person> ppl) {
-        return null;
+
+        return ppl.stream()
+                .filter(person -> person.getAge() < 18)
+                .map(person -> person.getName())
+                .collect(Collectors.toList());
     }
 
     public List<User> convertPeopleToUsers(List<Person> people) {
-        return null;
+
+        return people.stream()
+                .map(person -> new User(person.getName(), person.getAge(), "example nickname"))
+                .collect(Collectors.toList());
     }
 }
